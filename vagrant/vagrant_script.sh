7a81e5ee19c38512aa5580b313f78ee609907349
Steve Rowley <srowley@gmail.com>
2014-04-30 05:44:38 -0500

    added vagrant shell script

---
+++ vagrant/vagrant_script.sh
@@ -0,0 +1,8 @@
+echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main"| sudo tee -a /etc/apt/sources.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install curl git tmux vim postgresql-9.3 libpq-dev -y
+curl -sSL https://get.rvm.io | bash -s stable
+source /usr/local/rvm/scripts/rvm
+rvm use --install 2.1.1
+gem install rails --no-ri --no-rdoc
